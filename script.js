44b464b9f4c7d10a92e3f1c8c84ff5b4ee2828e8
banupriya1719 <banupriya17190209@gmail.com>
2025-03-18 14:44:28 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendvalue(value){
+ document.getElementById("display").value+=value;   
+}
+
+function cleardisplay(){
+ document.getElementById("display").value="";
+}
+
+function backspace(){
+    let displayvalue=document.getElementById("display");
+    displayvalue.value=displayvalue.value.slice(0,-1);
+}
+
+function calculateresult(){
+    try {
+        document.getElementById("display").value=eval(document.getElementById("display").value);
+    } catch (error) {
+        document.getElementById("display").value="Error";
+    }
+    
+}
